33ef26879c1bd3217fb03d17bb540574ebdca1b2
Szlesiowaa <jklonowski@mat.umk.pl>
2024-12-25 14:19:15 +0100

    Dodano sprawdzanie liczby pierwszej

--- projekt1.cpp
+++ projekt1.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
+bool czyPierwsza(int a){
+	float sa=sqrt(a);
+	for(int i=2;i<=sa;i++)if(a%i==0)return false;
+	return true;
+}
+
 int main(){
-//utworzenie projektu
+int a;
+cout<<"Podaj liczbe: ";
+cin>>a;
+cout<<czyPierwsza(a)<<endl;
 
 return 0;
 }
