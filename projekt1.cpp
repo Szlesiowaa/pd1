727920a46456b01fd0fb2d3bbcd2bd914c08eacc
Szlesiowaa <jklonowski@mat.umk.pl>
2024-12-25 14:22:35 +0100

    Dodano menu do funkcjonalnosci

--- projekt1.cpp
+++ projekt1.cpp
@@ -9,10 +9,24 @@ bool czyPierwsza(int a){
 }
 
 int main(){
-int a;
+int a,menu;
 cout<<"Podaj liczbe: ";
 cin>>a;
-cout<<czyPierwsza(a)<<endl;
+cout<<"Co mam zrobic z ta liczba? (1 - sprawdz czy pierwsza, 2 - oblicz kwadrat, 3 - oblicz pierwiastek kwadratowy): ";
+cin>>menu;
+switch(menu){
+	case 1:
+	cout<<czyPierwsza(a)<<endl;
+	break;
+	case 2:
+	cout<<a*a<<endl;
+	break;
+	case 3:
+	cout<<sqrt(a)<<endl;
+	break;
+	default:
+	cout<<"Nastepnym razem podaj poprawna opcje menu"<<endl;
+}
 
 return 0;
 }
