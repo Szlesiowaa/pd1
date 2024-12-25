716c7e68a16d2eb5a6c1444fb3d2a6967eba170c
Szlesiowaa <jklonowski@mat.umk.pl>
2024-12-25 14:12:49 +0100

    Dodano menu funkcjonalnosci

--- projekt1.cpp
+++ projekt1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int silnia(int liczba){
@@ -7,10 +8,24 @@ int silnia(int liczba){
 }
 
 int main(){
-int a;
+int a,menu;
 cout<<"Podaj liczbe: ";
 cin>>a;
-cout<<silnia(a)<<endl;
+cout<<"Co mam zrobic z ta liczba? (1 - oblicz silnie, 2 - oblicz kwadrat, 3 - oblicz pierwiastek kwadratowy): ";
+cin>>menu;
+switch(menu){
+	case 1:
+	cout<<silnia(a)<<endl;
+	break;
+	case 2:
+	cout<<a*a<<endl;
+	break;
+	case 3:
+	cout<<sqrt(a)<<endl;
+	break;
+	default:
+	cout<<"Nastepnym razem podaj poprawna opcje menu"<<endl;
+}
 
 return 0;
 }
