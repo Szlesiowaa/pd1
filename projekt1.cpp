8467d1dbc4300c829d6d200cfb1ba79152190b57
Szlesiowaa <jklonowski@mat.umk.pl>
2024-12-25 14:06:16 +0100

    Dodano funkcje obliczajaca silnie

--- projekt1.cpp
+++ projekt1.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int silnia(int liczba){
+	if(liczba==0 || liczba==1)return 1;
+	else return silnia(liczba-1)*liczba;
+}
+
 int main(){
-//utworzenie projektu
+int a;
+cout<<"Podaj liczbe: ";
+cin>>a;
+cout<<silnia(a)<<endl;
 
 return 0;
 }
